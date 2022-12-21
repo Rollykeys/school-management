89269246b687d76d680b7ebe42a00f1da375a651
Tapiwa Motsi <tmotsi10@gmail.com>
2022-12-21 13:31:25 +0200

    Roles Added

--- config/app.php
+++ config/app.php
@@ -198,6 +198,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
     ],
 
